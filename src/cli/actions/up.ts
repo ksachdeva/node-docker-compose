7c5747780e11e498bcd5ad735da0092848438fa3
Kapil Sachdeva <ksachdeva17@gmail.com>
2018-10-30 22:34:27 -0500

    feat - start to implement the support for 'up' command / add support to create networks

--- src/cli/actions/up.ts
+++ src/cli/actions/up.ts
@@ -1,4 +1,5 @@
 import {CommandLineAction} from '@microsoft/ts-command-line';
+import {Compose} from '../../compose';
 import {Project} from '../../types/project';
 import {AppCommandLine} from '../cmd-line';
 
@@ -15,8 +16,10 @@ export class UpAction extends CommandLineAction {
   protected onDefineParameters(): void {}
 
   protected async onExecute(): Promise<void> {
-    // build the project
     const project = new Project(this._parser.config);
+    const compose = new Compose(project);
+
+    await compose.up();
 
     return Promise.resolve();
   }
--- src/compose.ts
+++ src/compose.ts
@@ -2,7 +2,7 @@ import Docker from 'dockerode';
 import * as _ from 'lodash';
 
 import {validateComposeSpec} from './compose-spec-validator';
-import {Project} from './types';
+import {Network, Project} from './types';
 import {getOrderedServiceList} from './utils';
 
 export class Compose {
@@ -10,12 +10,21 @@ export class Compose {
   public constructor(private readonly project: Project) {
     // validate if the spec is correct
     validateComposeSpec(project);
-    this.docker = new Docker({ socketPath : '/var/run/docker.sock' });
+    this.docker = new Docker({socketPath: '/var/run/docker.sock'});
   }
 
   public async up(): Promise<void> {
     // get the ordered list
     const services = getOrderedServiceList(this.project.services);
+
+    // pull the images first before modifying/changing anything
+    // this way even if there are failures there is no n/w creation
+    // at this point of time
+    await this.pullImages();
+
+    // create the networks for the project
+    await this._createNetworks();
+
     return Promise.resolve();
   }
 
@@ -25,6 +34,41 @@ export class Compose {
       const pullStream = await this.docker.pull(s.imageName.name, {});
       await this._promisifyStream(pullStream);
     });
+  }
+
+  private async _createNetworks(): Promise<string[]> {
+    // figure out which networks already exist
+    // and only create the ones that do not
+    const networks = await this.docker.listNetworks();
+    // :( dockrode type definitions are seriously 'not good'
+    const existingNw: string[] = networks.map((n) => n.Name);
+    const networksToCreate: Network[] = [];
+
+    // Note - here I am matching the network name only
+    // As per the documentation network name is just a label
+    // the real deal is network Id
+    //
+    // As long as the network driver is bridge below code is
+    // valid. Once I have support for more network drivers then
+    // the comparison should be for full network object and not
+    // just the name
+
+    _.forEach(this.project.networks, (n) => {
+      if (!_.includes(existingNw, n.name.name)) {
+        networksToCreate.push(n);
+      }
+    });
+
+    // at present we create them sequentially
+    const nwIds: string[] = [];
+    _.forEach(networksToCreate, async (nw) => {
+      console.log(`Creating network ${nw.name.name}`);
+      const nwId = await this.docker.createNetwork(
+          {Name: nw.name.name, Driver: nw.driver});
+      nwIds.push(nwId);
+    });
+
+    return nwIds;
   }
 
   private _promisifyStream(stream: any) {
